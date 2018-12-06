10f40d5a2e437b79f8dc9b4d1db14f70d826ae91
Marat Gadzhibutaev <rabbitjkee@gmail.com>
2018-12-06 15:06:40 +0300

    added audio screen template

--- lib/ui/audio_screen.dart
+++ lib/ui/audio_screen.dart
@@ -3,8 +3,45 @@ import 'package:flutter/material.dart';
 class AudioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kTextTabBarHeight),
+          child: Container(
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Expanded(child: Container()),
+                  TabBar(
+                    tabs: [
+                      tabText("Лектор 1"),
+                      tabText("Лектор 2"),
+                      tabText("Лектор 3")
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Text(""),
+            Text(""),
+            Text(""),
+          ],
+        ),
+      ), // See the next step!
     );
   }
+}
+
+StatelessWidget tabText(String text) {
+  return Container(
+      padding: EdgeInsets.only(bottom: 8.0),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.blue, fontSize: 16.0),
+      ));
 }
