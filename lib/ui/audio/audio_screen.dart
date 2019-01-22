cad2d68bda4c4f41ca80370e6de2329f5a1a4b34
Ahrorus <ab.ahror@gmail.com>
2019-01-22 02:42:39 -0800

    Changed Stateless to Stateful Widget in AudioScreen and added three files per each lecturer tab

--- lib/ui/audio/audio_screen.dart
+++ lib/ui/audio/audio_screen.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
 
 import '../../util/constants.dart';
+import 'lector_one_tab.dart';
+import 'lector_two_tab.dart';
+import 'lector_three_tab.dart';
 
+
+class AudioScreen extends StatefulWidget {
+  @override
+  _AudioScreenState createState() => _AudioScreenState();
+}
+
+class _AudioScreenState extends State<AudioScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kTextTabBarHeight),
+          child: Container(
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Expanded(child: Container()),
+                  TabBar(
+                    tabs: [
+                      TabText(lecturers[0]),
+                      TabText(lecturers[1]),
+                      TabText(lecturers[2])
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            LectorOneTab(),
+            LectorTwoTab(),
+            LectorThreeTab(),
+          ],
+        ),
+      ), // See the next step!
+    );
+  }
+}
+
+
+/*
 class AudioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -38,6 +86,7 @@ class AudioScreen extends StatelessWidget {
     );
   }
 }
+*/
 
 class TabText extends StatelessWidget {
   final String text;
